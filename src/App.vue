ad9cfc3cf715c88b2ea4952dfc9bbca5fea0f004
dan-adekoya <60848550+dan-adekoya@users.noreply.github.com>
2020-11-02 19:26:19 +0100

    app

--- src/App.vue
+++ src/App.vue
@@ -12,15 +12,9 @@ export default {
 </script>
 
 <style>
-body{
-  background: black;
-}
-#app {
+*{
+  padding: 0;
+  margin: 0;
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
