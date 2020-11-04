6f91c320d831984c562668d1097f6bb7a1fe8cc0
dan-adekoya <60848550+dan-adekoya@users.noreply.github.com>
2020-11-04 16:23:30 +0100

    views

--- src/components/viewEmployee.vue
+++ src/components/viewEmployee.vue
@@ -1,12 +1,29 @@
 <template>
     <div>
         <h1>Employee</h1>
-        <h2>{{$route.params.employee_id}}</h2>
+        <h2>{{ route.params.employee_id }}</h2>
     </div>
 </template>
 
 <script>
+import { reactive, toRef } from 'vue'
+import { useRoute } from 'vue-router'
+export default{
+    setup(){
+        const state =  reactive({
+            em_id: null,
+            name: null,
+            dept: null,
+            role: null,
+        })
 
+        const route = useRoute()
+        return{
+            ...toRef(state),
+            route
+        }
+    }
+}
 </script>
 
 <style scoped>
